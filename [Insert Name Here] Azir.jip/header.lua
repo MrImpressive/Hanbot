5de26d6d3da91e2fd1954b2d9126eca0ae6f0155
MrImpressive <34780255+MrImpressive@users.noreply.github.com>
2018-01-25 11:42:13 -0600

    Delete header.lua

--- [Insert Name Here] Azir.jip/header.lua
+++
@@ -1,9 +0,0 @@
-return {
-  ["scriptType"] = "Champion",
-  ["scriptName"] = "[Insert Name Here] Azir",
-  ["moduleName"] = "[Insert_Name_Here]_Azir",
-  ["entryPoint"] = "main.lua",
-  ["loadToCoreMenu"] = function()
-    return player.charName == "Azir"
-  end
-}
