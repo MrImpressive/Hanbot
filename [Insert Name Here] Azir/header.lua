402a98bb987b62b7668158f46ddc717f334e7116
MrImpressive <34780255+MrImpressive@users.noreply.github.com>
2018-04-22 23:33:47 -0500

    Version 0.08

---
+++ [Insert Name Here] Azir/header.lua
@@ -0,0 +1,10 @@
+return {
+    id = 'insertnamehereazir',
+    name = '[Insert Name Here] Azir',
+    flag = {
+        text = 'Champion'
+    },
+    load = function()
+      return player.charName == 'Azir'
+    end
+}
