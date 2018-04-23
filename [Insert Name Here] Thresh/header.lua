6875fa578fdafc57d8888c074aa0561a2d50c638
MrImpressive <34780255+MrImpressive@users.noreply.github.com>
2018-04-22 23:29:35 -0500

    Version 0.05

---
+++ [Insert Name Here] Thresh/header.lua
@@ -0,0 +1,10 @@
+return {
+    id = 'insertnameherethresh',
+    name = '[Insert Name Here] Thresh',
+    flag = {
+        text = 'Champion'
+    },
+    load = function()
+      return player.charName == 'Thresh'
+    end
+}
